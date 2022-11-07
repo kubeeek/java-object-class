16fb53df68feb2889405f0d653f6baa36e0ab9e4
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2022-11-07 21:27:25 +0100

    changed main method

--- lab1/src/main/java/agh/ics/oop/World.java
+++ lab1/src/main/java/agh/ics/oop/World.java
@@ -1,29 +1,38 @@
 package agh.ics.oop;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class World {
-    static HashSet<String> allowedArgs = new HashSet<>(Arrays.asList("f", "r", "d", "l"));
-
     public static void main(String[] args) {
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
+        /*
+            duplikacje zwierząt można sprawdzić za pomocą np. HashSetu, gdzie kluczem byłyby współrzędne czyli wektor.
+            Jeżeli klucz istnieje w zbiorze, to znaczy, że jest tam zwierzę.
+         */
+        Animal animal = new Animal();
+        System.out.println(animal);
+
+        List<MoveDirection> directions = OptionsParser.parse(args);
+
+        for (MoveDirection direction :
+                directions) {
+            run(animal, direction);
+        }
+        System.out.println(animal);
     }
 
-    public static void run(Direction direction) {
+    public static void run(Animal animal, MoveDirection direction) {
         String message = switch (direction) {
-            case FORWARD -> "do przodu";
-            case BACKWARD -> "do tyłu";
-            case RIGHT -> "w prawo";
-            case LEFT -> "w lewo";
+            case FORWARD -> "idzie do przodu";
+            case BACKWARD -> "idzie do tyłu";
+            case RIGHT -> "obraca się w prawo";
+            case LEFT -> "obraca się w lewo";
         };
 
-        String line = String.format("Zwierze idzie %s.", message);
+        animal.move(direction);
+        String line = String.format("Zwierze %s.", message);
         System.out.println(line);
     }
 }
