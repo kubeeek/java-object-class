fea71d136aba14bd723cf189ddb375ab6bd3ed95
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2022-11-07 21:25:08 +0100

    refactor

--- lab1/src/main/java/agh/ics/oop/Animal.java
+++ lab1/src/main/java/agh/ics/oop/Animal.java
@@ -22,18 +22,18 @@ public class Animal {
             case FORWARD -> {
                 Vector2d newPosition = this.position.add(this.heading.toUnitVector());
                 if(!(newPosition.x > 4 || newPosition.y > 4 || newPosition.x < 0 || newPosition.y < 0))
-                    this.position = this.position.add(this.heading.toUnitVector());
+                    this.position = newPosition;
             }
             case BACKWARD -> {
                 Vector2d newPosition = this.position.add(this.heading.toUnitVector().opposite());
                 if(!(newPosition.x > 4 || newPosition.y > 4 || newPosition.x < 0 || newPosition.y < 0))
-                    this.position = this.position.add(this.heading.toUnitVector());
+                    this.position = newPosition;
             }
         }
     }
 
     boolean isAt(Vector2d position) {
-        return position == this.position;
+        return position.equals(this.position);
     }
 
     @Override
